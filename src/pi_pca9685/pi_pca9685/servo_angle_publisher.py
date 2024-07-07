2a74aba26fafd9a05359dda270ac2479bbce0202
Your Name <you@example.com>
2024-07-07 13:26:23 +1000

    1123

--- src/pi_pca9685/pi_pca9685/servo_angle_publisher.py
+++ src/pi_pca9685/pi_pca9685/servo_angle_publisher.py
@@ -9,29 +9,29 @@ class KeyboardPublisher(Node):
         super().__init__('keyboard_publisher')
         self.publisher_ = self.create_publisher(String, 'servo_commands', 10)
         # Initialize angles as specified
-        self.angles = [122, 150, 57, 30, 122, 150, 57, 30]
-        self.angle_step = 10  # Step to decrease angles
+        self.angles = [120, 150, 60, 30, 120, 150, 60, 30]
+        # self.angle_step = 20  # Step to decrease angles
         self.lift_angle_PIN15 = 110
         self.default_angle_PIN15 = 150
 
-        self.forward_angle_PIN0 = 150
-        self.default_angle_PIN0 = 57
+        self.forward_angle_PIN0 = 170
+        self.default_angle_PIN0 = 120
         self.back_angle_PIN0 = 70
 
-        self.forward_angle_PIN4 = 110
-        self.default_angle_PIN4 = 57
-        self.back_angle_PIN4 = 70
+        self.forward_angle_PIN4 = 70
+        self.default_angle_PIN4 = 120
+        self.back_angle_PIN4 = 170
 
         self.lift_angle_PIN37 = 70
         self.default_angle_PIN37 = 30
 
-        self.forward_angle_PIN2 = 70
-        self.default_angle_PIN2 = 57
-        self.back_angle_PIN2 = 110
+        self.forward_angle_PIN2 = 110
+        self.default_angle_PIN2 = 60
+        self.back_angle_PIN2 = 10
 
-        self.forward_angle_PIN6 = 110
-        self.default_angle_PIN6 = 57
-        self.back_angle_PIN6 = 70
+        self.forward_angle_PIN6 = 10
+        self.default_angle_PIN6 = 60
+        self.back_angle_PIN6 = 110
 
     def publish_angles(self):
         msg = String()
@@ -41,62 +41,57 @@ class KeyboardPublisher(Node):
     def decrease_angles(self):
         self.publish_angles()
         while not (self.angles[1] == self.lift_angle_PIN15 and self.angles[0] == self.forward_angle_PIN0 ):
-            if self.angles[1] > self.lift_angle_PIN15:
-                self.angles[1] = self.angles[1] - self.angle_step
+            if self.angles[1] != self.lift_angle_PIN15:
+                self.angles[1] = self.lift_angle_PIN15
                 self.publish_angles()
-            elif self.angles[0] > self.forward_angle_PIN0:
-                self.angles[0] = self.angles[0] - self.angle_step
+                time.sleep(1)
+            elif self.angles[0] != self.forward_angle_PIN0:
+                self.angles[0] = self.forward_angle_PIN0
                 self.publish_angles()
+                time.sleep(1)
             else:
                 self.get_logger().info("Lift Leg 1 and Move the Leg 1")
                 break
-
+        
         while not (self.angles[1] == self.default_angle_PIN15 ):
-            if self.angles[1] < self.default_angle_PIN15:
-                self.angles[1] = self.angles[1] + self.angle_step
+            if self.angles[1] != self.default_angle_PIN15:
+                self.angles[1] = self.default_angle_PIN15
                 self.publish_angles()
+                time.sleep(1)
             else:
                 self.get_logger().info("Leg 1 Touch the ground")
                 break
 
         while not (self.angles[5] == self.lift_angle_PIN15 and self.angles[4] == self.forward_angle_PIN4 ):
-            if self.angles[5] > self.lift_angle_PIN15:
-                self.angles[5] = self.angles[5] - self.angle_step
+            if self.angles[5] != self.lift_angle_PIN15:
+                self.angles[5] = self.lift_angle_PIN15
                 self.publish_angles()
-            elif self.angles[4] < self.forward_angle_PIN4:
-                self.angles[4] = self.angles[4] + self.angle_step
+                time.sleep(1)
+            elif self.angles[4] != self.forward_angle_PIN4:
+                self.angles[4] = self.forward_angle_PIN4
                 self.publish_angles()
+                time.sleep(1)
             else:
                 self.get_logger().info("Lift Leg 3 and Move the Leg 3")
                 break
 
         while not (self.angles[5] == self.default_angle_PIN15 ):
-            if self.angles[5] < self.default_angle_PIN15:
-                self.angles[5] = self.angles[5] + self.angle_step
+            if self.angles[5] != self.default_angle_PIN15:
+                self.angles[5] = self.default_angle_PIN15
                 self.publish_angles()
+                time.sleep(1)
             else:
                 self.get_logger().info("Leg 3 Touch the ground")
                 break
 
         while not (self.angles[0] == self.default_angle_PIN0 and self.angles[4] == self.default_angle_PIN4
                    and self.angles[2] == self.back_angle_PIN2 and self.angles[6] == self.back_angle_PIN6 ):
-            if  self.angles[0] < self.default_angle_PIN0:
-                self.angles[0] = self.angles[0] + self.angle_step
+                self.angles[0] = self.default_angle_PIN0
+                self.angles[4] = self.default_angle_PIN4
+                self.angles[2] = self.back_angle_PIN2
+                self.angles[6] = self.back_angle_PIN6
                 self.publish_angles()
-
-            elif self.angles[4] > self.default_angle_PIN4:
-                self.angles[4] = self.angles[4] - self.angle_step
-                self.publish_angles()
-
-            elif self.angles[2] < self.back_angle_PIN2:
-                self.angles[2] = self.angles[2] + self.angle_step
-                self.publish_angles()
-
-            elif self.angles[6] > self.back_angle_PIN6:
-                self.angles[6] = self.angles[6] - self.angle_step
-                self.publish_angles()
-
-            else:
+                time.sleep(1)
                 self.get_logger().info("Move Leg 0 and Move the Leg 2 Move Leg 4 and Move the Leg 6")
                 break
 
